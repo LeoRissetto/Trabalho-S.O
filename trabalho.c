02e5261cfef56687c5098f07c9ea1f8487b3b59a
Leonardo Gueno Rissetto <leonardorissetto01@gmail.com>
2024-04-04 10:21:25 -0300

    Codigo Base
    fiz a base com base nas intrucoes do texto, porem ainda esta longe de funcionar

--- trabalho.c
+++ trabalho.c
@@ -2,67 +2,45 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 #define TRUE 1
 #define THREAD_NUM 5
 
-sem_t semEmpty;
-sem_t semFull;
+sem_t semMateriaEmpty;
+sem_t semMateriaFull;
 
-pthread_mutex_t mutexBuffer;
+sem_t semCanetaEmpty;
+sem_t semCanetaFull;
 
-int buffer[10];
-int count = 0;
+pthread_mutex_t mutexMateria;
 
-int timeWait = 0;
-int timeMake = 0;
-int timeSend = 0;
+pthread_mutex_t mutexCaneta;
+
+int maxStoreMateria = 100;
+int maxStoreCaneta = 100;
+
+int tempoEspera = 0;
+int tempoEnvio = 0;
+int tempoFazer = 0;
 int qntCanetas = 0;
 int qntEnviado = 0;
 int qntMateria = 0;
-int maxStore = 0;
-
-void *producer(void* args){
-
-    while(TRUE){
-
-        int x = rand() % 100;
-
-        sleep(1);
-
-        sem_wait(&semEmpty);
-        pthread_mutex_lock(&mutexBuffer);
-        buffer[count] = x;
-        count++;
-        pthread_mutex_unlock(&mutexBuffer);
-        sem_post(&semFull);
-    }
-}
-
-void *consumer(void *args){
-
-    while(TRUE){
-
-        int y;
-
-        sem_wait(&semFull);
-        pthread_mutex_lock(&mutexBuffer);
-        y = buffer[count - 1];
-        count--;
-        pthread_mutex_unlock(&mutexBuffer);
-        sem_post(&semEmpty);
-
-        printf("Consumiu %d\n", y);
-
-        sleep(1);
-    }
-}
 
 //thread rank 1
 void *depositoMateria(void *args){
 
     while(TRUE){
 
+        sem_wait(&semMateriaEmpty);
+        pthread_mutex_lock(&mutexMateria);
+        qntMateria--;
+        pthread_mutex_unlock(&mutexMateria);
+        sem_post(&semMateriaFull);
+
+        printf("Enviou materia prima\n");
+
+        sleep(tempoEnvio);
     }
 }
 
@@ -71,6 +49,15 @@ void *celulaFabricacao(void *args){
 
     while(TRUE){
 
+        sem_wait(&semMateriaFull);
+        pthread_mutex_lock(&mutexMateria);
+        // Fabricar canetas
+        pthread_mutex_unlock(&mutexMateria);
+        sem_post(&semMateriaEmpty);
+
+        printf("Produziu caneta\n");
+
+        sleep(tempoFazer);
     }
 }
 
@@ -87,6 +74,13 @@ void *depositoCanetas(void *args){
 
     while(TRUE){
 
+        sem_wait(&semCanetaEmpty);
+        pthread_mutex_lock(&mutexCaneta);
+        qntCanetas++;
+        pthread_mutex_unlock(&mutexCaneta);
+        sem_post(&semCanetaFull);
+
+        printf("Armazenou caneta\n");
     }
 }
 
@@ -95,26 +89,46 @@ void *comprador(void *args){
 
     while(TRUE){
 
+        sem_wait(&semCanetaFull);
+        pthread_mutex_lock(&mutexCaneta);
+        qntCanetas++;
+        pthread_mutex_unlock(&mutexCaneta);
+        sem_post(&semCanetaEmpty);
+
+        printf("Consumiu caneta\n");
+
+        sleep(tempoEspera);
     }
 }
 
-int main(){
+int main(int argc, char* argv[]){
 
     pthread_t th[THREAD_NUM];
-    pthread_mutex_init(&mutexBuffer, NULL);
 
-    sem_init(&semEmpty, 0, 10);
-    sem_init(&semFull, 0, 0);
+    pthread_mutex_init(&mutexMateria, NULL);
 
-    int i = pthread_create(&th[1], NULL, &depositoMateria, NULL);
-    i += pthread_create(&th[2], NULL, &celulaFabricacao, NULL);
-    i += pthread_create(&th[3], NULL, &controle, NULL);
-    i += pthread_create(&th[4], NULL, &depositoCanetas, NULL);
-    i += pthread_create(&th[5], NULL, &comprador, NULL);
+    pthread_mutex_init(&mutexCaneta, NULL);
 
-    if(i != 0){
-        perror("Falha ao criar thread");
-    }
+    sem_init(&semMateriaEmpty, 0, maxStoreMateria);
+    sem_init(&semMateriaFull, 0, 0);
+
+    sem_init(&semCanetaEmpty, 0, maxStoreCaneta);
+    sem_init(&semCanetaFull, 0, 0);
+
+    if(pthread_create(&th[1], NULL, &depositoMateria, NULL) != 0)
+        perror("Falha ao criar thread\n");
+
+    if(pthread_create(&th[2], NULL, &celulaFabricacao, NULL) != 0)
+        perror("Falha ao criar thread\n");
+
+    if(pthread_create(&th[3], NULL, &controle, NULL) != 0)
+        perror("Falha ao criar thread\n");
+
+    if(pthread_create(&th[4], NULL, &depositoCanetas, NULL) != 0)
+        perror("Falha ao criar thread\n");
+
+    if(pthread_create(&th[5], NULL, &comprador, NULL) != 0)
+        perror("Falha ao criar thread\n");
 
     for(int i =0; i < THREAD_NUM; i++){
         if(pthread_join(th[i], NULL) != 0){
@@ -122,10 +136,15 @@ int main(){
         }
     }
 
-    sem_destroy(&semEmpty);
-    sem_destroy(&semFull);
+    sem_destroy(&semMateriaEmpty);
+    sem_destroy(&semMateriaFull);
 
-    pthread_mutex_destroy(&mutexBuffer);
+    sem_destroy(&semCanetaEmpty);
+    sem_destroy(&semCanetaFull);
+
+    pthread_mutex_destroy(&mutexMateria);
+
+    pthread_mutex_destroy(&mutexCaneta);
 
     return 0;
 }
